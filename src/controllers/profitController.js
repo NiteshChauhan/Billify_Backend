6fe460895a7a7b1e660aa9f834bf3c55cc005164
Niteshpal Singh <niteshsingh22002@gmail.com>
2026-04-14 23:01:25 +0530

    Correct P&L Today Report

--- src/controllers/profitController.js
+++ src/controllers/profitController.js
@@ -1,30 +1,40 @@
 const { getProfitSummary } = require("../utils/profitUtils");
-const { getDateRangeFromQuery } = require("../utils/dateRange");
+
+const endOfDay = (date) => {
+  const d = new Date(date);
+  d.setHours(23, 59, 59, 999);
+  return d;
+};
+
+const startOfDay = (date) => {
+  const d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  return d;
+};
+
+const isValidDate = (value) => !Number.isNaN(new Date(value).getTime());
 
 exports.getProfit = async (req, res) => {
   try {
-    const { range, mode } = req.query;
+    const { range, mode, from, to, fyStart, fyEnd } = req.query;
     const companyId = req.user.companyId;
 
     const now = new Date();
     let fromDate;
     let toDate = now;
 
-    /* ---------- CUSTOM DATE RANGE ---------- */
-    const explicitRange = getDateRangeFromQuery(req.query);
-    if (explicitRange) {
-      fromDate = explicitRange.fromDate;
-      toDate = explicitRange.toDate;
-    } else {
-
-    /* ---------- PREDEFINED RANGES ---------- */
+    /* ---------- DATE RANGE PRECEDENCE ---------- */
+    if (from && to && isValidDate(from) && isValidDate(to)) {
+      fromDate = startOfDay(from);
+      toDate = endOfDay(to);
+    } else if (range === "today") {
+      fromDate = startOfDay(now);
+      toDate = endOfDay(now);
+    } else if (range) {
+      /* ---------- PREDEFINED RANGES ---------- */
       fromDate = new Date();
 
       switch (range) {
-        case "today":
-          fromDate.setHours(0, 0, 0, 0);
-          break;
-
         case "week":
           fromDate.setDate(now.getDate() - 7);
           break;
@@ -46,6 +56,12 @@ exports.getProfit = async (req, res) => {
         default:
           fromDate.setHours(0, 0, 0, 0);
       }
+    } else if (fyStart && fyEnd && isValidDate(fyStart) && isValidDate(fyEnd)) {
+      fromDate = startOfDay(fyStart);
+      toDate = endOfDay(fyEnd);
+    } else {
+      fromDate = startOfDay(now);
+      toDate = endOfDay(now);
     }
 
     const includeEntries = mode === "entries";
