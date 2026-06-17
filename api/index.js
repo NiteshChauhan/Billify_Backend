4ce8f303e8bc300296fc3fc1e87bf67e882cf60f
Niteshpal Singh Chouhan <niteshsingh22002@gmail.com>
2026-06-17 16:26:11 +0530

    Correct Url in index

--- api/index.js
+++ api/index.js
@@ -14,6 +14,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:3000",
+  "https://billifyadmin.vercel.app",
+
 ]
   .flatMap((value) => String(value || "").split(","))
   .map((origin) => origin.trim())
